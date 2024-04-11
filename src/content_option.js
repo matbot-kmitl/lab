00b9cdad93b2783cd3cdec871e281886e7850c7e
Piyawud Koonmanee <piyawudkoon@hotmail.com>
2024-04-11 18:10:44 +0700

    update files

---
+++ src/content_option.js
@@ -0,0 +1,44 @@
+const logotext = "MATBOT KMITL";
+const meta = {
+    title: "MATBOT Lab | Smart Materials & Robotics AI Engineering, KMITL",
+    description: "MATBOT Lab - A webserver lab for MATBOTians",
+};
+
+const introdata = {
+    title: "Welcome to MATBOT Lab",
+    title2: "a place to",
+    animated: {
+        first: "experiment",
+        second: "share",
+        third: "have fun",
+    },
+    description: "MATBOTians can experiment and deploy their project without paying a single cent. We hope you enjoy your visit!",
+};
+
+// Start Edit here
+const dataproject = [{
+    img: "./projects/micro/cover.png",
+    desctiption: "Micro's Portfolio.",
+    link: "https://matbot.kmitl.ac.th/lab/micro/",
+},
+    // {
+    //     img: "./projects/second_project/cover.png",
+    //     desctiption: "Second Project",
+    //     link: "https://matbot.kmitl.ac.th/lab/second_project/",
+    // },
+];
+// Stop Edit here
+
+const socialprofils = {
+    mainweb: "https://matbot.kmitl.ac.th",
+    instagram: "https://www.instagram.com/matbot.official/",
+    facebook: "https://www.facebook.com/CMIT.KMITL/",
+};
+
+export {
+    meta,
+    dataproject,
+    introdata,
+    socialprofils,
+    logotext,
+};
